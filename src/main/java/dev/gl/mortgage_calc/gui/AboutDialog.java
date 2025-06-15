d3acfac440b81bc553af0c2f27f901e882e33f90
gl <g.lipovtsev@gmail.com>
2025-06-16 00:26:13 +0300

    AboutDialog updated

--- src/main/java/dev/gl/mortgage_calc/gui/AboutDialog.java
+++ src/main/java/dev/gl/mortgage_calc/gui/AboutDialog.java
@@ -21,7 +21,7 @@ import javax.swing.KeyStroke;
  * @author gl
  */
 public class AboutDialog extends javax.swing.JDialog {
-    
+
     private AbstractAction okButtonAction;
 
     public AboutDialog(java.awt.Frame parent, boolean modal) {
@@ -36,7 +36,8 @@ public class AboutDialog extends javax.swing.JDialog {
         getVersion();
         setHomeLink();
         setMortgageSourceLink();
-//        setLibrarySourceLink();
+        setLibrary1SourceLink();
+        setLibrary2SourceLink();
     }
 
     @SuppressWarnings("unchecked")
@@ -63,7 +64,10 @@ public class AboutDialog extends javax.swing.JDialog {
         jLabel8 = new javax.swing.JLabel();
         jPanel7 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
-        zXingLinkLabel = new javax.swing.JLabel();
+        apachePOILinkLabel = new javax.swing.JLabel();
+        jPanel8 = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
+        jFreeChartLinkLabel = new javax.swing.JLabel();
         bottomPanel = new javax.swing.JPanel();
         okButton = new javax.swing.JButton();
 
@@ -81,7 +85,7 @@ public class AboutDialog extends javax.swing.JDialog {
 
         jPanel4.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 0, 5));
 
-        programNameLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        programNameLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         programNameLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         programNameLabel.setText("Mortgage Calculator");
         jPanel4.add(programNameLabel);
@@ -90,12 +94,12 @@ public class AboutDialog extends javax.swing.JDialog {
 
         jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 0, 5));
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         jLabel3.setText("Version: ");
         jPanel3.add(jLabel3);
 
-        versionLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        versionLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         versionLabel.setText("jLabel4");
         jPanel3.add(versionLabel);
 
@@ -109,11 +113,11 @@ public class AboutDialog extends javax.swing.JDialog {
 
         jPanel1.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel4.setText("Home: ");
         jPanel1.add(jLabel4);
 
-        homeLinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        homeLinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         homeLinkLabel.setText("jLabel5");
         homeLinkLabel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
         jPanel1.add(homeLinkLabel);
@@ -122,11 +126,11 @@ public class AboutDialog extends javax.swing.JDialog {
 
         jPanel2.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel6.setText("Mortgage image: ");
         jPanel2.add(jLabel6);
 
-        mortgageLinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        mortgageLinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         mortgageLinkLabel.setText("jLabel7");
         mortgageLinkLabel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
         jPanel2.add(mortgageLinkLabel);
@@ -135,7 +139,7 @@ public class AboutDialog extends javax.swing.JDialog {
 
         jPanel6.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
 
-        jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel8.setText("Dependencies: ");
         jPanel6.add(jLabel8);
 
@@ -143,21 +147,35 @@ public class AboutDialog extends javax.swing.JDialog {
 
         jPanel7.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel5.setText("1. LibraryName: ");
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel5.setText("1. Apache POI: ");
         jPanel7.add(jLabel5);
 
-        zXingLinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        zXingLinkLabel.setText("jLabel5");
-        zXingLinkLabel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
-        jPanel7.add(zXingLinkLabel);
+        apachePOILinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        apachePOILinkLabel.setText("jLabel5");
+        apachePOILinkLabel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        jPanel7.add(apachePOILinkLabel);
 
         linksPanel.add(jPanel7);
+
+        jPanel8.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel7.setText("2. JFreeChart 1.5.3:  ");
+        jPanel8.add(jLabel7);
+
+        jFreeChartLinkLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jFreeChartLinkLabel.setText("jLabel5");
+        jFreeChartLinkLabel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        jPanel8.add(jFreeChartLinkLabel);
+
+        linksPanel.add(jPanel8);
 
         mainPanel.add(linksPanel, java.awt.BorderLayout.SOUTH);
 
         getContentPane().add(mainPanel, java.awt.BorderLayout.CENTER);
 
+        okButton.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         okButton.setText("OK");
         bottomPanel.add(okButton);
 
@@ -167,13 +185,16 @@ public class AboutDialog extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel apachePOILinkLabel;
     private javax.swing.JPanel bottomPanel;
     private javax.swing.JLabel homeLinkLabel;
     private javax.swing.JPanel infoPanel;
+    private javax.swing.JLabel jFreeChartLinkLabel;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
@@ -181,6 +202,7 @@ public class AboutDialog extends javax.swing.JDialog {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
+    private javax.swing.JPanel jPanel8;
     private javax.swing.JPanel linksPanel;
     private javax.swing.JPanel logoPanel;
     private javax.swing.JPanel mainPanel;
@@ -189,7 +211,6 @@ public class AboutDialog extends javax.swing.JDialog {
     private javax.swing.JLabel programNameLabel;
     private javax.swing.JPanel upperSubpanel;
     private javax.swing.JLabel versionLabel;
-    private javax.swing.JLabel zXingLinkLabel;
     // End of variables declaration//GEN-END:variables
 
     private void loadLogo() {
@@ -243,29 +264,49 @@ public class AboutDialog extends javax.swing.JDialog {
             }
         });
     }
-    
-    
-//    private void setLibrarySourceLink() {
-//        final String imageLink = "https://github.com/zxing/zxing";
-//        final String githubLink = """
-//                                <html>
-//                                <a href="https://github.com/zxing/zxing">ZXing</a>
-//                                , licensed under Apache License 2.0
-//                                </html>
-//                                """;
-//        zXingLinkLabel.setText(githubLink);
-//        zXingLinkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
-//        zXingLinkLabel.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//                try {
-//                    Desktop.getDesktop().browse(new URI(imageLink));
-//                } catch (Exception urlException) {
-//                }
-//            }
-//        });
-//    }
-    
+
+    private void setLibrary1SourceLink() {
+        final String imageLink = "https://github.com/apache/poi";
+        final String githubLink = """
+                                <html>
+                                <a href="https://github.com/apache/poi">Apache POI</a>
+                                , licensed under Apache License 2.0
+                                </html>
+                                """;
+        apachePOILinkLabel.setText(githubLink);
+        apachePOILinkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        apachePOILinkLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI(imageLink));
+                } catch (Exception urlException) {
+                }
+            }
+        });
+    }
+
+    private void setLibrary2SourceLink() {
+        final String imageLink = "https://github.com/jfree/jfreechart";
+        final String githubLink = """
+                                <html>
+                                <a href="https://github.com/jfree/jfreechart">JFreeChart</a>
+                                , licensed under LGPL-2.1
+                                </html>
+                                """;
+        jFreeChartLinkLabel.setText(githubLink);
+        jFreeChartLinkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        jFreeChartLinkLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI(imageLink));
+                } catch (Exception urlException) {
+                }
+            }
+        });
+    }
+
     private void attachListenerToOkButton() {
         okButton.addActionListener(okButtonAction);
     }
@@ -273,13 +314,13 @@ public class AboutDialog extends javax.swing.JDialog {
     private void bindKeyListenersToOkButton() {
         InputMap inputMap = this.getRootPane().getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
         ActionMap actionMap = this.getRootPane().getActionMap();
-        
+
         inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "ok");
         inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "ok");
-        
+
         actionMap.put("ok", okButtonAction);
     }
-    
+
     private void setupIcon() {
         ImageIcon icon = new ImageIcon(this.getClass().getClassLoader().getResource("images/icons8-mortgage-40.png"));
         this.setIconImage(icon.getImage());
